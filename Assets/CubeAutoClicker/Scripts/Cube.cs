266023e6b130af6295a0cd387826283728ed0e26
Kieran <73572378+KieranBDW@users.noreply.github.com>
2021-03-14 04:31:08 +1000

    Kieran Night 4am
    Got code for cubes done. Need animation done and hero's done.

--- Assets/CubeAutoClicker/Scripts/Cube.cs
+++ Assets/CubeAutoClicker/Scripts/Cube.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cube : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int cubeRow;
+    private int cubeColumn;
+    private Color cubeUntouchedColor;
+    private Color cubeFinalColor;
+    private int moneyDropped;
+    private int cubeHealth;
+    private bool cubeBroke;
+    private int MaxHealthOfCube;
+    private Color CubeColorIncrease;
+    public TextMesh MoneyTextDesplay;
+
+    // Gets Rigidbody
+    private Rigidbody rb;
+
+    [SerializeField] private Material cubeMaterial;
+    Renderer rend;
+
+    public void CubeSetup(int _Row, int _Col, Color _FinalColor, int _Health, Color _UntouchColor, Material _CubeMaterial)
     {
-        
+        cubeRow = _Row;
+        cubeColumn = _Col;
+        cubeFinalColor = _FinalColor;
+        MaxHealthOfCube = _Health;
+        cubeUntouchedColor = _UntouchColor;
+        cubeHealth = MaxHealthOfCube; ;
+        CubeColorIncrease = (cubeFinalColor - cubeUntouchedColor) / (cubeHealth - 1);
+
+        // Getting rigidbody
+        rb = GetComponent<Rigidbody>();
+
+        // Getting materials
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = _CubeMaterial;
+
+        rend.material.color = cubeUntouchedColor;
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool Click()
     {
-        
+        cubeHealth--;
+        if (cubeHealth <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            CubeHit();
+            return false;
+        }
+    }
+
+    public void Break(int _MoneyDrop)
+    {
+        rend.material.color = cubeUntouchedColor;
+        cubeHealth = MaxHealthOfCube;
+        // Will print the 3d $$ once broken.
+        // Vector3 force = cubeRow * cubeColumn;
+        // TextDisplay = ("$$ " + IntToString(_MoneyDrop));
+    }
+
+    private void CubeHit()
+    {
+        rend.material.color += CubeColorIncrease;
     }
 }
---
+++ Assets/CubeAutoClicker/Scripts/MoneyText.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyText : MonoBehaviour
+{
+    // Will move up and then disapear after 2 seconds.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
