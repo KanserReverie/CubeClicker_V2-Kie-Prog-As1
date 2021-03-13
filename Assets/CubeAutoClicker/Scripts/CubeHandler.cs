202385d20644022af3c8aaa9608b8384952228cd
Kieran <73572378+KieranBDW@users.noreply.github.com>
2021-03-14 00:53:51 +1000

    Next update

--- Assets/CubeAutoClicker/Scripts/CubeHandler.cs
+++ Assets/CubeAutoClicker/Scripts/CubeHandler.cs
@@ -7,12 +7,63 @@ public class CubeHandler : MonoBehaviour
     private int RowMax = 3;
     private int ColMax = 4;
     private float cubeDistanceBetweenCubes = 2;
-    private Cube[,] cubeGrid = new Cube[3,4];//[Row] [Col]
+    private GameObject[,] cubeGrid = new GameObject[3, 4];//[Row] [Col]
     private int baseMoneyForBreakingCube = 8;
     private int baseHealthOfCubes = 8;
-    public Color[][] CubeGridColors = new Color[3][4];// -- NEEDED
+    public Color[,] CubeGridColors = new Color[3, 4];// -- NEEDED
     public Money MoneyCount;
-    public Cube CubePrefab;
+    public GameObject CubePrefab;
     public Vector3 StartPointCubeGrid;
 
+   /*
+    private void Start()
+    {
+        GameObject newCube = new GameObject();
+        Vector3 extraDistance = new Vector3(0, 0, 0);
+
+        for (int y = 0; y < RowMax; y++)
+        {
+            for (int x = 0; x < ColMax; x++)
+            {
+                extraDistance = new Vector3(x * cubeDistanceBetweenCubes, y * cubeDistanceBetweenCubes, 0);
+                newCube = Instantiate(CubePrefab, transform.position + extraDistance, Quaternion.identity, transform);
+                newCube.Setup(y, x, CubeGridColors[y, x], baseHealthOfCubes);
+                cubeGrid[y, x] = newCube;
+            }
+        }
+    }
+
+    public void Click()
+    {
+        bool CubeDone = false;
+        int CurRow = 0;
+        int CurCol = 0;
+        int moneyMade = 0;
+
+        do
+        {
+            CubeDone = cubeGrid[CurRow, CurCol].Click();
+
+            if (CubeDone == true)
+            {
+                moneyMade = CubeBreakMoney(CurRow, CurCol);
+                cubeGrid[CurRow, CurCol].Break(moneyMade);
+                CurCol++;
+
+                if (CurCol > ColMax)
+                {
+                    CurCol = 0;
+                    CurRow++;
+                }
+            }
+        } while (CubeDone == true);
+    }
+
+    private int CubeBreakMoney(int _CubeRow, int _CubeCol)
+    {
+        int MoneyGained = ((((_CubeRow) * ColMax) + (_CubeCol + 1)) * baseMoneyForBreakingCube);
+        MoneyCount.MoneyGained(MoneyGained);
+        return MoneyGained;
+    }
+    */
 }
--- Assets/CubeAutoClicker/Scripts/Money.cs
+++ Assets/CubeAutoClicker/Scripts/Money.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Money : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int money;
+    public Text moneyDisplay;
+    private string pricingText = "$$";
+
+    private void Start()
     {
-        
+        money = 0;
+    }
+    private void Update()
+    {
+        moneyDisplay.text = pricingText + money.ToString("N0");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoneyGained(int _q1)
     {
-        
+        money += _q1;
+    }
+
+    public void MoneyLost(int _q2)
+    {
+        money -= _q2;
+    }
+
+    public int GetMoney()
+    {
+        return money;
     }
 }
